3503e9754049e4471d9096387248bb5d6f887169
caiovictormc <caiovictor31@live.com>
2018-09-26 20:42:54 -0300

    Periodic function to update virtual LED status

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@
 
 #include <Glacier/Glacier.h>
 
-Glacier glacier;
 
+WidgetLED virtualLED(V1);
+
+Glacier glacier;
+BlynkTimer timer;
+
+
+// Auth Settings
 char auth[] = "1b1c946ad73c4d38800d7685651c9491";
 char ssid[] = "Venal Ninja Labs";
 char pass[] = "Venal@2016";
-
 // char ssid[] = "d0f2a4";
 // char pass[] = "";
 
@@ -23,10 +28,20 @@ BLYNK_WRITE(V1) {
 	glacier.powerOff();
 }
 
+
+void sendStatus() {
+	if (glacier.isOn() == true) {
+		virtualLED.on();
+	} else {
+		virtualLED.off();
+	}
+}
+
 void setup() {
 	Serial.begin(9600);
   	Blynk.begin(auth, ssid, pass, "git.canionlabs.io", 8080);
   	glacier.begin();
+  	timer.setInterval(5000L, sendStatus);
 }
 
 void loop() {
